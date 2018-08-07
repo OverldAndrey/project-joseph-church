d0f3a2c35d79b2290516fdab02f77d1f8d7f8913
iai17u666 <andrey3000.99@mail.ru>
2018-08-07 22:00:05 +0300

    add user login function prototype

--- web-app/joseph/joseph_app/urls.py
+++ web-app/joseph/joseph_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
+app_name="joseph_app"
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
+    path('login/', views.login_user, name="login_user")
 ]
--- web-app/joseph/joseph_app/views.py
+++ web-app/joseph/joseph_app/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, logout, login
 from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hale Joseph")
+
+def login_user(request):
+    email = request.POST["email"]
+    password = request.POST["password"]
+    user = authenticate(request, username=email, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponse("Login successful")
+    else:
+        return HttpResponse("Login failed")
 
 # Create your views here.
