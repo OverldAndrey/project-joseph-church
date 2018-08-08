fe262dd73481d5599ce7a49dd38421297b035fe2
iai17u666 <andrey3000.99@mail.ru>
2018-08-08 20:47:57 +0300

    add user modification

--- web-app/joseph/joseph_app/urls.py
+++ web-app/joseph/joseph_app/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
     path('registration/', views.create_user, name="create_user"),
     path('reg_new_user/', views.register_user, name="register_user"),
+    path('user_<int:user_pk>/profile/update', views.redact_user, name="redact_user"),
+    path('user_<int:user_pk>/update', views.update_user, name="update_user"),
+    path('user_<int:user_pk>/profile', views.user_cab, name="user_cab"),
     path('login/', views.login_user, name="login_user")
 ]
--- web-app/joseph/joseph_app/views.py
+++ web-app/joseph/joseph_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, logout, login
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponsePermanentRedirect
+from django.urls import reverse
 
 from .models import User
 
@@ -13,12 +14,12 @@ def login_user(request):
     user = authenticate(request, username=email, password=password)
     if user is not None:
         login(request, user)
-        return HttpResponse("Login successful")
+        return HttpResponsePermanentRedirect(reverse("joseph_app:user_cab", args=(user.pk,)))
     else:
         return HttpResponse("Login failed")
 
 def create_user(request):
-    return render(request, "registration/new_user.html")
+    return render(request, "registration/update_user.html")
 
 def register_user(request):
     user = User.objects.create_user(email=request.POST['email'], password=request.POST['password'])
@@ -36,6 +37,46 @@ def register_user(request):
     user.organizations = request.POST['organizations']
     user.hobby = request.POST['hobby']
     user.save()
-    return HttpResponse("Registered")
+    return HttpResponsePermanentRedirect(reverse("joseph_app:login_user"))
+
+def redact_user(request, user_pk):
+    user = request.user
+    if user is not None and user.is_authenticated and user.pk == user_pk:
+        response = {
+            "user" : user,
+        }
+        return render(request, "registration/update_user.html", response)
+
+def update_user(request, user_pk):
+    user = request.user
+    if user is not None and user.is_authenticated and user.pk == user_pk:
+        user.name = request.POST['name']
+        user.surname = request.POST['surname']
+        user.second_name = request.POST['second_name']
+#        user.avatar =
+        dob = request.POST['date_of_birth']
+        user.date_of_birth = dob[6:]+"-"+dob[3:5]+"-"+dob[:2]
+        user.phone = request.POST['phone']
+        user.pubnet = request.POST['pubnet']
+        user.university = request.POST['university']
+        user.course = request.POST['course']
+        user.reg_address = request.POST['reg_address']
+        user.cur_address = request.POST['cur_address']
+        user.organizations = request.POST['organizations']
+        user.hobby = request.POST['hobby']
+        user.save()
+        return HttpResponsePermanentRedirect(reverse("joseph_app:user_cab", args=(user.pk,)))
+
+def user_cab(request, user_pk):
+    user = request.user
+    if user is not None and user.is_authenticated and user.pk == user_pk:
+        response = {
+            "user" : user,
+        }
+        return render(request, "joseph_app/cabinet.html", response)
+    else:
+        return HttpResponsePermanentRedirect(reverse("joseph_app:login"))
+
 
 # Create your views here.
+
